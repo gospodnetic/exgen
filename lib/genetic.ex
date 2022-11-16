0c26818bacf8d02db1985c430e06441d8f467753
Petra Gospodnetic <petra.gospodnetic@itwm.fraunhofer.de>
2022-11-16 19:21:08 +0100

    Generalization to Problem and Speller

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -2,6 +2,8 @@ defmodule Genetic do
   @moduledoc """
   Documentation for `Genetic`.
   """
+  alias Types.Chromosome
+
   def initialize(genotype, opts \\ []) do
     population_size = Keyword.get(opts, :population_size, 100)
     for _ <- 1..population_size, do: genotype.()
@@ -9,7 +11,13 @@ defmodule Genetic do
 
   def evaluate(population, fitness_function, opts \\ []) do
     population
-    |> Enum.sort_by(fitness_function, &>=/2)
+    |> Enum.map(
+      fn chromosome ->
+        fitness = fitness_function.(chromosome)
+        age = chromosome.age + 1
+        %Chromosome{chromosome | fitness: fitness, age: age}
+      end)
+    |> Enum.sort_by(& &1.fitness, &>=/2)
   end
 
   def select(population, opts \\ []) do
@@ -22,11 +30,13 @@ defmodule Genetic do
     population
     |> Enum.reduce([],
       fn {p1, p2}, acc ->
-        cx_point = :rand.uniform(length(p1))
+        cx_point = :rand.uniform(length(p1.genes))
         {{h1, t1}, {h2, t2}} =
-          {Enum.split(p1, cx_point),
-          Enum.split(p2, cx_point)}
-        {c1, c2} = {h1 ++ t2, h2 ++ t1}
+          {Enum.split(p1.genes, cx_point),
+          Enum.split(p2.genes, cx_point)}
+        {c1, c2} =
+          {%Chromosome{p1 | genes: h1 ++ t2},
+          %Chromosome{p2 | genes: h2 ++ t1}}
         [c1, c2 | acc]
       end)
   end
@@ -36,32 +46,32 @@ defmodule Genetic do
     |> Enum.map(
       fn chromosome ->
         if :rand.uniform() < 0.05 do
-          Enum.shuffle(chromosome)
+          %Chromosome{chromosome | genes: Enum.shuffle(chromosome.genes)}
+          chromosome
         else
           chromosome
         end
-      end
-    )
+      end)
   end
 
-  def run(fitness_function, genotype, max_fitness, opts \\ []) do
-    population = initialize(genotype)
+  def run(problem, opts \\ []) do
+    population = initialize(&problem.genotype/0)
     population
-    |> evolve(fitness_function, max_fitness, opts)
+    |> evolve(problem, opts)
   end
 
-  def evolve(population, fitness_function, max_fitness, opts \\ []) do
-    population = evaluate(population, fitness_function, opts)
+  def evolve(population, problem, opts \\ []) do
+    population = evaluate(population, &problem.fitness_function/1, opts)
     best = hd(population)
-    IO.write("\rCurrent best: #{fitness_function.(best)}")
-    if fitness_function.(best) == max_fitness do
+    IO.write("\rCurrent best: #{best.fitness}")
+    if problem.terminate?(population) do
       best
     else
       population
       |> select(opts)
       |> crossover(opts)
       |> mutation(opts)
-      |> evolve(fitness_function, max_fitness, opts)
+      |> evolve(problem, opts)
     end
   end
 
